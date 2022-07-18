c2668e36370a3363e466b92cdd547093c7b73cb9
Melvin Bustamante <87199037+meljavi@users.noreply.github.com>
2022-07-18 16:41:00 -0600

    Update DrSdkPlugin.java

--- android/src/main/java/com/dr/plugin/dr_sdk_plugin/DrSdkPlugin.java
+++ android/src/main/java/com/dr/plugin/dr_sdk_plugin/DrSdkPlugin.java
@@ -8,23 +8,19 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 
-import amazonia.iu.com.amlibrary.activities.DRComplianceActivity;
 import amazonia.iu.com.amlibrary.client.IUApp;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-
+import android.util.Log;
 /** DrSdkPlugin */
 public class DrSdkPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+
   private MethodChannel channel;
   private static Application application;
-  private static DRComplianceActivity drComplianceActivity;
+  private static Class<?> drComplianceActivity;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
@@ -32,7 +28,7 @@ public class DrSdkPlugin implements FlutterPlugin, MethodCallHandler {
     channel.setMethodCallHandler(this);
   }
 
-  public static void init(Application application,DRComplianceActivity drComplianceActivity) {
+  public static void init(Application application,Class<?> drComplianceActivity) {
     DrSdkPlugin.application = application;
     DrSdkPlugin.drComplianceActivity=drComplianceActivity;
   }
@@ -41,6 +37,7 @@ public class DrSdkPlugin implements FlutterPlugin, MethodCallHandler {
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
 
     switch (call.method){
+
       case "getPlatformVersion":
         result.success("Android " + android.os.Build.VERSION.RELEASE);
         break;
@@ -48,11 +45,13 @@ public class DrSdkPlugin implements FlutterPlugin, MethodCallHandler {
         IUApp.refreshFCMToken(application);
         break;
       case "invokeFCM":
+        Log.d("TestIU","DrSdkPlugin call invokeFCM()");
         try{
+        IUApp.init(application, drComplianceActivity);
         HashMap<String, Object> hashMap = (HashMap<String, Object>) call.arguments;
         HashMap<String, Object> finalMap=  (HashMap<String, Object>)hashMap.get("remoteMessage");
-          JSONObject json =new JSONObject(finalMap);
-          IUApp.handleFCMMessage(application, json);
+        JSONObject json =new JSONObject(finalMap);
+        IUApp.handleFCM(application, json);
         }catch (Exception e){
           e.printStackTrace();
         }
@@ -61,7 +60,8 @@ public class DrSdkPlugin implements FlutterPlugin, MethodCallHandler {
         }
         break;
       case "launch":
-        IUApp.init(application, null);
+        Log.d("TestIU","DrSdkPlugin call init()");
+        IUApp.init(application, drComplianceActivity);
         break;
       case "init":
         IUApp.launch(application);
